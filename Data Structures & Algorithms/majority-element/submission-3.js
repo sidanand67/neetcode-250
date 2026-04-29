ac7fc92d01b6c9a44bd5148970b2c41cf8739bc2
Sidharth Anand <sidharthanand67@gmail.com>
2026-04-29 18:56:55 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    majorityElement(nums) {
+        let count = 0; 
+        let candidate = null; 
+        for(let num of nums) {
+            if (count === 0) {
+                candidate = num; 
+            }
+            if (candidate === num) {
+                count++; 
+            } else {
+                count--; 
+            }
+        }
+        return candidate ;
+    }
+}
